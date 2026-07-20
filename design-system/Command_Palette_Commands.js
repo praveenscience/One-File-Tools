2c065f08b83f3d9789f774c81766a58501cf8117
CoderforLife-AK <thefirstians1@gmail.com>
2026-07-20 09:42:00 +0530

    Create Command_Palette_Commands.js

---
+++ design-system/Command_Palette_Commands.js
@@ -0,0 +1,18 @@
+export const commands = [
+  {
+    group: "Pages",
+    icon: "🏠",
+    title: "Dashboard",
+    description: "Go to dashboard",
+    shortcut: "G D",
+    action: () => alert("Dashboard")
+  },
+  {
+    group: "Pages",
+    icon: "📦",
+    title: "Components",
+    description: "Browse UI components",
+    shortcut: "G C",
+    action: () => alert("Components")
+  }
+];
