e9c6c686890e8f0f274b498c8c1db80b25557828
CoderforLife-AK <thefirstians1@gmail.com>
2026-07-20 09:43:28 +0530

    Create Components_Renderer.js

---
+++ design-system/Components_Renderer.js
@@ -0,0 +1,12 @@
+export function renderCommands(groups) {
+    // Creates grouped command lists
+
+    // Pages
+    // Actions
+    // Settings
+    // Recent
+
+    // Adds icons
+    // descriptions
+    // shortcut badges
+}
